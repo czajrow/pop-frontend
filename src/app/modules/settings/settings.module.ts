408231db805a82727638c0c5f298c187c2882b21
Adam Czajka <czajka.adam147@gmail.com>
2020-11-05 18:01:14 +0100

    Create diagram component

--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import {SectionModule} from '../../shared/section/section.module';
+import { DiagramComponent } from './components/diagram/diagram.component';
 
 
 
 @NgModule({
-    declarations: [SettingsComponent],
+    declarations: [SettingsComponent, DiagramComponent],
     exports: [
         SettingsComponent
     ],
